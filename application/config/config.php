3eaca17ac709571d77a8fc0ade36201bd7fb3af4
vanpersietyo <barcelonitas.adhyt@gmail.com>
2019-11-21 22:54:28 +0700

    ganti config dinamic

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://popjasa.nuhapos.com/';
+$root = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = "$root";
 
 /*
 |--------------------------------------------------------------------------
