1134939c325d0952a3536dc545a4908eb7f19848
sanaya <afes.okta@gmail.com>
2020-02-12 22:07:57 +0700

    perbaikan master fix asset & transaksi fix assets

--- application/views/transaksi/trs_asset/list.php
+++ application/views/transaksi/trs_asset/list.php
@@ -4,7 +4,7 @@ $this->load->view('template/head');
 
 <div class="content-header row">
     <div class="content-header-left col-md-6 col-12 mb-2">
-        <h3 class="content-header-title">Fix assets List</h3>
+        <h3 class="content-header-title"></h3>
         <div class="row breadcrumbs-top">
             <div class="breadcrumb-wrapper col-12">
                 <ol class="breadcrumb">
@@ -21,8 +21,10 @@ $this->load->view('template/head');
 <div class="row">
     <div class="col-lg-6 col-md-6">
         <div class="form-group">
-            <button type="button" class="btn mb-1 btn-info btn-lg btn-block pull-up" onclick="add()"><a> <i
-                            class="la la-plus"></i> Tambah Fix Assets </a></button>
+            <a class="btn mb-1 btn-info btn-lg btn-block pull-up"
+               href="<?php echo site_url('transaksi/trs_asset/create') ?>"><i class="ft-excell"></i> Tambah Fix
+                Asset</a>
+
         </div>
     </div>
 
@@ -44,10 +46,10 @@ $this->load->view('template/head');
                         <br>
                         <!-- Invoices List table -->
                         <div class="table-responsive">
-                            <table class="table table-bordered table-striped" id="mytable">
+                            <table id="mytable" class="table table-striped table-bordered sourced-data">
                                 <thead>
                                 <tr>
-                                    <th width="80px">No</th>
+                                    <th width="80px">TrNo</th>
                                     <th>Fa Id</th>
                                     <th>Jenis</th>
                                     <th>Date Beli</th>
@@ -70,65 +72,19 @@ $this->load->view('template/head');
     </section>
 </div>
 
+
 <?php
 $this->load->view('/template/foot');
 ?>
 
-<script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
-<script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
-<script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
 <script type="text/javascript">
     $(document).ready(function () {
-        $.fn.dataTableExt.oApi.fnPagingInfo = function (oSettings) {
-            return {
-                "iStart": oSettings._iDisplayStart,
-                "iEnd": oSettings.fnDisplayEnd(),
-                "iLength": oSettings._iDisplayLength,
-                "iTotal": oSettings.fnRecordsTotal(),
-                "iFilteredTotal": oSettings.fnRecordsDisplay(),
-                "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
-                "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
-            };
-        };
-
         var t = $("#mytable").dataTable({
-            initComplete: function () {
-                var api = this.api();
-                $('#mytable_filter input')
-                    .off('.DT')
-                    .on('keyup.DT', function (e) {
-                        if (e.keyCode == 13) {
-                            api.search(this.value).draw();
-                        }
-                    });
+            "ajax": {
+                "url": "<?php echo site_url('transaksi/trs_asset/json')?>",
+                "type": "POST"
             },
-            oLanguage: {
-                sProcessing: "loading..."
-            },
-            processing: true,
-            serverSide: true,
-            ajax: {"url": "trs_asset/json", "type": "POST"},
-            columns: [
-                {
-                    "data": "trno",
-                    "orderable": false
-                }, {"data": "fa_id"}, {"data": "jenis"}, {"data": "date_beli"}, {"data": "estimasi"}, {"data": "date_penyusutan"}, {"data": "hrg_beli"}, {"data": "penyusutan_thn"}, {"data": "penyusutan_bln"}, {"data": "pembulatan"},
-                {
-                    "data": "action",
-                    "orderable": false,
-                    "className": "text-center"
-                }
-            ],
-            order: [[0, 'desc']],
-            rowCallback: function (row, data, iDisplayIndex) {
-                var info = this.fnPagingInfo();
-                var page = info.iPage;
-                var length = info.iLength;
-                var index = page * length + (iDisplayIndex + 1);
-                $('td:eq(0)', row).html(index);
-            }
         });
     });
 </script>
-
 
