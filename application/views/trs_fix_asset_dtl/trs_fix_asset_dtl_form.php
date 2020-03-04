a9cbc7f13ae72986b64bb622f92d4ddfa49099cb
afes Oktavianus <afes.okta@gmail.com>
2020-03-04 11:12:30 +0700

    POP Jasa V.1.0 04-03-2020: 1. perbaikan untuk pembulatan, sebelumnya dibulatkan dengan precision 4 sekarang menjadi 2

--- application/views/trs_fix_asset_dtl/trs_fix_asset_dtl_form.php
+++ application/views/trs_fix_asset_dtl/trs_fix_asset_dtl_form.php
@@ -60,7 +60,7 @@
                                 <div class="form-group">
                                     <label for="decimal">Hrg Beli <?php echo form_error('Hrg_beli') ?></label>
                                     <input type="text" class="form-control" name="Hrg_beli" id="Hrg_beli"
-                                           placeholder="Hrg Beli" value="<?php echo $Hrg_beli; ?>" readonly/>
+                                           placeholder="Hrg Beli" value="<?php echo $Hrg_beli; ?>"/>
                                 </div>
                                 <?php if ($status == 'Update') { ?>
                                     <div class="form-group">
@@ -122,7 +122,6 @@
                                                             <th style="min-width: 10%">Kode FA</th>
                                                             <th style="min-width: 15%">Nama Item</th>
                                                             <th style="min-width: 15%">Tanggal Beli</th>
-                                                            <th style="min-width: 15%">Harga Beli</th>
                                                             <th style="min-width: 5%">Aksi</th>
                                                         </tr>
                                                         </thead>
