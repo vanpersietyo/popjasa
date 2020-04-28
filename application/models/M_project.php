42acc84693a56545920190bc09b6eac69abe5866
AfesOkta <afes.okta@gmail.com>
2020-04-28 22:30:15 +0700

    POPJASA V1.0.1 - 28-04-2020 - Tambahkan order by tgl_input desc ditampilan progres project

--- application/models/M_project.php
+++ application/models/M_project.php
@@ -341,7 +341,7 @@ class M_project extends CI_Model {
         $query = $this->db->query("
 				select a.id_project,a.kd_cabang,a.id_customer,b.nm_customer,a.harga_jual,a.keterangan,a.input_by,a.tgl_input,a.st_data,a.st_project
 				from trs_project a
-				JOIN m_customer b ON a.id_customer=b.id_customer and a.st_data=1
+				JOIN m_customer b ON a.id_customer=b.id_customer and a.st_data=1 ORDER BY a.tgl_input desc
 			");
         return $query->result();
     }
