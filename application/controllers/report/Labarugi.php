c6228d2abe57daa80282dbc4e71d13e762dc73be
AfesOkta <afes.okta@gmail.com>
2020-05-10 22:17:31 +0700

    POPJasa - V1.0.1 - 10-05-2020 - Perbaikan spasi ziz(zakat.... menjadi ziz (zakat....

--- application/controllers/report/Labarugi.php
+++ application/controllers/report/Labarugi.php
@@ -253,7 +253,7 @@ class Labarugi extends CI_Controller
         $pdf->Cell(10, 5, '', 0, 1);
         $pdf->Cell(10, 5, '', 0, 1);
         $pdf->SetTextColor(255,255,255);
-        $pdf->Cell(87, 5, 'BIAYA ZIS(ZAKAT / INFAQ / SODAQOH)', 1, 0, 'L',true);
+        $pdf->Cell(87, 5, 'BIAYA ZIS (ZAKAT / INFAQ / SODAQOH)', 1, 0, 'L',true);
         $prosentase_5 = $total_omz2 ? $sumzis / $total_omz2 * 100 : 0;
         $echo_p5 = number_format($prosentase_5);
         $pdf->Cell(30, 5, "$echo_p5 %", 1, 1, 'R',true);
